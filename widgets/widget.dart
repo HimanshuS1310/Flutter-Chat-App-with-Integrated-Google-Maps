27a56dd50d00aacf5e63aeb9876a2185d705996a
HIMANSHU SHARMA <52231652+HimanshuS1310@users.noreply.github.com>
2020-11-19 13:33:26 +0530

    Add files via upload

---
+++ widgets/widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Widget appbar(BuildContext context){
+  return AppBar(
+    title: Image.asset("assets/logo.png"),
+    backgroundColor: Colors.white,
+  );
+}
